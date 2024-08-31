7477b8a85e103a455a07dce7fdc477f5d24c3629
widmaamelia <widmaamelia@gmail.com>
2024-08-31 17:01:08 +0700

    kalkulatorsederhana

---
+++ app/src/main/java/com/ilham/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.ilham.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnKalkulatorSederhana : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnKalkulatorSederhana = findViewById(R.id.btnKalkulatorSederhana)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnKalkulatorSederhana.setOnClickListener() {
+            val intentMenu4 = Intent(this@MainActivity, KalkulatorSederhanaActivity::class.java)
+            startActivity(intentMenu4)
+        }
+    }
+    }
